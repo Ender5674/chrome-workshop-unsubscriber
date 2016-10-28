9cda6b6e5f217c05ddbe2e15b3cb02a5aa8e792c
PatrickJoannisse <patick.joannisse@gmail.com>
2016-10-27 21:46:51 -0400

    Improve style and add some comments

--- unsubscriber.js
+++ unsubscriber.js
@@ -1,15 +1,42 @@
 (function() {
 
 function showButton() {
-
+	//Where we want to add our button
+    var parentDiv = document.getElementById('rightContents');
+	//Our button
 	var div = document.createElement('div');
+	//Text of said button
+	var text = document.createTextNode('Unsubscribe All');
+	//We'll need Font-Awesome
+	var link = document.createElement('link');
 
-	var style = "position: absolute; top: 0px; left: 0px; width: 30px; height: 30px; background-color: #fff";
+	link.setAttribute('href','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+	link.setAttribute('rel','stylesheet');
+	link.setAttribute('integrity','sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN');
+	link.setAttribute('crossorigin','anonymous');
 
-	div.setAttribute("style", style);
+	//Append Font Awesome's Stylesheet to our page
+	document.head.appendChild(link);
+
+	//Create icon
+	var icon = document.createElement('i');
+
+	//Add style to our icon
+	icon.className += "fa fa-trash-o trash-icon";
+
+
+	//Append text node to the button
+	div.appendChild(text);
+	//Append icon to the button
+	div.appendChild(icon);
+
+    //Set a button id
 	div.setAttribute("id", "mass-unsubscribe-button");
-	
-	document.body.appendChild(div);
+	//Add style to our button
+    div.className += " unsubscribeBtn";
+
+	//Add our button to the page as a first element
+	parentDiv.insertBefore(div,parentDiv.firstChild);
 }
 
 function getIdFromUnsubscribeId(id) {
@@ -18,12 +45,12 @@ function getIdFromUnsubscribeId(id) {
 }
 
 function GETparse() {
-	
+
 	var queryDict = {}
 	location.search.substr(1).split("&").forEach(function(item) {
 		queryDict[item.split("=")[0]] = item.split("=")[1]
 	});
-	
+
 	return queryDict;
 }
 
@@ -39,28 +66,28 @@ function startUnsubscribing(event) {
 		ids.push(getIdFromUnsubscribeId(docs[i].id));
 		d.click();
 	}
-	
+
 	var count = ids.length;
-	
+
 	var timer = setInterval(function() {
 		for (var i = 0; i < ids.length; i++) {
 			var unsubId = 'Unsubscribed' + ids[i];
-		
+
 			var unsubMsg = document.querySelector('#' + unsubId + ' .workshopItemUnsubscribedDetails div');
-			
+
 			if (unsubMsg) {
 				count--;
 			}
 		}
-		
+
 		if (count == 0) {
 			clearInterval(timer);
 			console.log('Everything unsubscribed');
 			location.reload();
 		} else {
 			console.log('Still unsubscribing');
-		}		
-		
+		}
+
 	}, 1000);
 }
 
